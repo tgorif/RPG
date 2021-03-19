47bb55edbc8e0b17091f3199350e6eda3e415d0f
tgorif <kay.st.janssen@googlemail.com>
2021-03-19 16:15:35 +0100

    wip

--- src/RPG/Main/Level.java
+++ src/RPG/Main/Level.java
@@ -2,13 +2,15 @@ package RPG.Main;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class Level {
     int[] x=new int[2];
     int[] y=new int[2];
     int[] z=new int[2];
+    public Map<List<Integer>,Tile> tiles=new HashMap<>();
+    private final Logger LOGGER = Logger.getLogger(Level.class.getName());
     static Level currentLevel;
     public Level(){
         x[0]=0;
@@ -17,6 +19,22 @@ public class Level {
         y[1]=1000;
         z[0]=0;
         z[1]=1000;
+        currentLevel=this;
+    }
+    public Level(int diameter,int height){
+        if(diameter%2==0)LOGGER.log(java.util.logging.Level.SEVERE,"created map with even diameter");
+        for(int h=0;h<height;h++) {
+            for (int i = diameter / (-2); i < diameter / 2; i++) {
+                for (int j = diameter / (-2); j < diameter / 2; j++) {
+                    for (int k = diameter / (-2); k < diameter / 2; k++) {
+                        if (i + j + k == h){
+                            Tile t = new Tile(i,j,k,h);
+                            tiles.put(t.toList(),t);
+                        }
+                    }
+                }
+            }
+        }
         currentLevel=this;
     }
     public static Level getCurrentLevel(){
@@ -65,5 +83,58 @@ public class Level {
     public boolean isEqualPosition(Position a,Position b){
         if(a.x!=b.x || a.y!=b.y || a.z!=b.z) return false;
         return true;
+    }
+    public List<Tile> getTilesInRange(Tile t,int distance){
+        List<Tile> result =new ArrayList<>();
+        for(int i=t.x-(distance/2);i<t.x+(distance/2);i++){
+            for(int j=t.y-(distance/2);j<t.y+(distance/2);j++){
+                for(int k=t.z-(distance/2);k<t.z+(distance/2);k++){
+                    if(i+j+k==t.level){
+                        List<Integer> tmp = List.of(i,j,k,t.level);
+                        if(tiles.containsKey(tmp)){
+                            result.add(tiles.get(tmp));
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    class Tile{
+        final int x;
+        final int y;
+        final int z;
+        final int level;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Tile tile = (Tile) o;
+            return x == tile.x && y == tile.y && z == tile.z && level == tile.level;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x*7, y*31, z*71, level*47);
+        }
+        public List<Integer> toList(){
+            return List.of(x,y,z,level);
+        }
+        public int getDistance(Tile other){
+            return  (Math.abs(this.x-other.x)
+                    +Math.abs(this.y-other.y)
+                    +Math.abs(this.z-other.z))
+                    /2+(this.level-other.level);
+        }
+
+        private Tile(int x,int y,int z,int level){
+            this.x=x;
+            this.y=y;
+            this.z=z;
+            this.level=level;
+            if(x+y+z!=level){
+                LOGGER.log(java.util.logging.Level.SEVERE,"created unreachable Tile " + x + " " + y + " " + level  +" " + z);
+            }
+        }
     }
 }
